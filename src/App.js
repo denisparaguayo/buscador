becfd85bb38e7fd0c2634d4cfdb491037160a87a
denisparaguayo <72168757+denisparaguayo@users.noreply.github.com>
2021-04-17 01:22:50 -0400

    consumiendo api de Pixab

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
-
+import React, { Component} from 'react';
 import './App.css';
+import Buscador from './componentes/Buscador';
+import Resultado from './componentes/Resultado';
 
-function App() {
-  return (
-    <div className="app container">
-      <div className="jumbotron">
-        <p  className="lead text-center">Buscador de Imágenes</p>
+class App extends Component {
 
+
+  state = {
+    termino : '',
+    imagenes : []
+  }
+
+  paginaAnterior = () =>{
+
+  }
+
+  paginaSiguiente = () =>{
+    
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const url = `https://pixabay.com/api/?key=15022583-2891b7e1735571b5ee32f9e27&q=${termino}&per_page=30`;
+    
+    fetch(url)
+      .then(respuesta => respuesta.json () )
+      .then(resultado => this.setState({imagenes : resultado.hits}))
+
+
+  }
+  
+  datosBusqueda = (termino) =>{
+    this.setState({
+      termino
+    }, () => {
+      this.consultarApi();
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="app container">
+        <div className="jumbotron">
+          <p  className="lead text-center">Buscador de Imágenes</p>
+  
+          <Buscador 
+          
+          datosBusqueda={this.datosBusqueda}
+          />
+  
+        </div>
+        <div className="row justify-content-center">
+
+        <Resultado
+          imagenes={this.state.imagenes}
+        />
+
+        </div>
       </div>
-    </div>
-  );
+    );
+
+  }
 }
 
 export default App;
--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -1,3 +1,41 @@
-import React, { component } from 'react';
+import React, { Component } from 'react';
 
-class
+class buscador extends Component {
+    
+    busquedaRef = React.createRef();
+
+
+    obtrenerDatos = (e) =>{        
+        e.preventDefault();
+        //Tomamos el Valor del Imput
+       const termino = this.busquedaRef.current.value;
+
+       //lo enviamos al componente Principal
+        this.props.datosBusqueda(termino);
+
+    }
+    
+    render() { 
+        return ( 
+            <form onSubmit={this.obtrenerDatos}>
+                <div className="row">
+                    <div className="form-group col-md-8">
+                        <input 
+                        ref={this.busquedaRef}
+                        type="text" 
+                        className="form-control form-control-lg" 
+                        placeholder="Busca tu Imagen. Ej: Fútbol" />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <input 
+                        type="submit" 
+                        className="btn btn-lg btn-danger btn-block" 
+                        value="Buscar..." />
+                    </div>
+                </div>
+            </form>
+         );
+    }
+}
+ 
+export default buscador;
